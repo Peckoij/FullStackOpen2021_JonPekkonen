4d94f9ba4ce97d4426a7cbbde03c6563bd160065
Peckoij <jonpekkonen@gmail.com>
2021-10-30 10:58:47 +0300

    Tehty tehtävän 3.11 loppuun

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+// heroku const baseUrl = 'https://puhelinluettelo-backend-peckoi.herokuapp.com/api/persons'
+// url for prod build, where react front is in backends root dir
+const baseUrl = '/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
